39d8cc51d98309a48c6b537b8bb3f9bc07ed044e
Micahtech <wisdomjohn1919@gmail.com>
2025-04-23 18:25:17 +0100

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import styles from "../style";
 import LetsConnect from "./LetsConnect";
 import Lottie from "react-lottie-player";
-import animationData from "../lotties/avatar.png";
+import animationData from "../lotties/person-coding.json";
 import { aboutMe } from "../constants";
 
 
