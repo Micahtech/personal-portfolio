611152d210471ef20aff4179d0de4489c3820da6
Micahtech <wisdomjohn1919@gmail.com>
2025-04-23 20:52:49 +0100

    skills

--- src/constants/index.js
+++ src/constants/index.js
@@ -71,13 +71,15 @@ import {
   SiOpenai,
   SiGooglecloud,
   SiNextdotjs,
-  SiMicrosoftazure
+  SiMicrosoftazure,
+  SiWoo
 } from "react-icons/si";
 
 import {
   FaHardHat,
   FaRust,
-  FaPhp
+  FaPhp,
+  FaWordpress
 } from "react-icons/fa";
 
 import {
@@ -85,7 +87,8 @@ import {
 } from "react-icons/io";
 
 import {
-  FaGolang
+  FaGolang,
+  FaNode
 } from "react-icons/fa6";
 
 
@@ -105,10 +108,7 @@ export const navLinks = [
     id: "education",
     title: "Education",
   },
-  {
-    id: "achievements",
-    title: "Achievements",
-  },
+ 
   {
     id: "projects",
     title: "Projects",
@@ -132,110 +132,23 @@ export const educationList = [
   {
     id: "education-1",
     icon: nitk,
-    title: "National Institute of Technology Karnataka, Surathkal",
-    degree: "Bachelor of Technology",
+    title: "University of Uyo, Akwa Ibom",
+    degree: "Bachelor of Computer Science",
     duration: "December 2020 - May 2024",
-    content1: "Major: Electronics and Communication Engineering",
+    content1: "Major: Software Development",
     content2: "Minor: Information Technology",
   },
   {
     id: "education-2",
     icon: pba,
-    title: "Polkadot Blockchain Academy",
+    title: "Voice of Liberty Training Schools",
     degree: "",
-    duration: "May 2024 - June 2024",
-    content1: "Graduated with a distinction in the fifth cohort of the Polkadot Blockchain Academy at the National University of Singapore.",
+    duration: "May 2020 - June 2021",
+    content1: "Trained and developed expertise in software development and programming.",
   },
 ];
 
 // Add your past achievments here for example - rankings in hackathons/events
-export const achievements = [
-  {
-    id: "a-1",
-    icon: ethindia,
-    event: "ETHIndia'24 | India's Largest Ethereum Hackathon",
-    position: "Winner",
-    content1: "True Network's Winner, Polkadot's 1st Runner Up",
-    content2: "Lit Protocol's 2nd Runner Up",
-    content3: "Coinbase Pool Prize, Base Top 10",
-    article: "https://www.linkedin.com/posts/mittal-parth_super-stoked-to-announce-that-our-team-emerged-activity-7274735259621961729-tkq4?utm_source=share&utm_medium=member_desktop",
-    project: "https://devfolio.co/projects/khoj-3336",
-    youtube: "https://www.youtube.com/live/qJ4OCtnvjUY?si=VkcnHEdwJTEEDlMg&t=4718",
-  },
-  {
-    id: "a-2",
-    icon: ethglobal,
-    event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
-    position: "Winner",
-    content1: "Top 12 winners among 20k+ registrations",
-    content2: "One of Polygon's Best Public Goods",
-    content3: "Best Module on Biconomy SDK",
-    article: "https://www.thehindu.com/news/cities/Mangalore/nitk-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
-    project: "https://devfolio.co/projects/chargeswap-3527",
-    youtube: "https://youtu.be/9rieTya8Yds?t=3908",
-  },
-  {
-    id: "a-3",
-    icon: polkadot,
-    event: "Polkadot Hackathon: Europe Edition",
-    position: "2nd Runner Up in the ink! Smart Contract Category",
-    content1: "Built GreenTrust offering a novel solution for obtaining certification in organic farming.",
-    content2: "",
-    content3: "",
-    github: "https://github.com/pranav2305/GreenTrust",
-  },
-  {
-    id: "a-4",
-    icon: lightspeed,
-    event: "Warpspeed by Lightspeed",
-    position: "1st Runner Up",
-    content1: "1st Runner Up Overall by Lightspeed among 107 hackers",
-    content2: "Top 3 projects using Replit",
-    content3: "1st Runner Up by Amazon Web Services (AWS)",
-    article: "https://shorturl.at/fhjsT",
-  },
-  {
-    id: "a-5",
-    icon: dennisivy,
-    event: "September Hackathon by Dennis Ivy",
-    position: "Winner",
-    content1: "Rated the best portfolio website among 450+ participants across the globe.",
-    content2: "",
-    content3: "",
-    youtube: "https://www.youtube.com/watch?v=X2473En3h_o&t=5278s",
-    project: "https://parthmittal.netlify.app/",
-  },
-  {
-    id: "a-6",
-    icon: manipal,
-    event: "Manipal Hackathon'22",
-    position: "Consolation Prize",
-    content1: "Top 10 among 500+ teams across India",
-    content2: "Developed a cross-platform mobile application to address the problem of social cohesion.",
-    content3: "",
-    article: "https://shorturl.at/exEIQ",
-  },
-  {
-    id: "a-7",
-    icon: icon,
-    event: "ICON Hyperbuild Hackathon",
-    position: "Honorable Mention",
-    content1: "Honorable Mention among 655 participants across the globe in a 3 month-long online hackathon.",
-    content2: "",
-    content3: "",
-    project: "https://devpost.com/software/green-trust-xj2w6g",
-  },
-  {
-    id: "a-8",
-    icon: ethforall,
-    event: "ETHForAll 2023",
-    position: "Top 3 Superfluid Projects",
-    content1: "Bounty winners among 430 projects in ETHGlobal's largest online hackathon.",
-    content2: "",
-    content3: "",
-    project: "https://devfolio.co/projects/green-trust-ed14",
-  },
-];
 
 // Add your software developments skills here for example - programming languages, frameworks etc.
 export const skills = [
@@ -243,19 +156,14 @@ export const skills = [
     title: "Programming Languages",
     items: [
       {
-        id: "pl-1",
-        icon: DiRuby,
-        name: "Ruby",
-      },
-      {
         id: "pl-2",
         icon: SiPython,
         name: "Python",
       },
       {
         id: "pl-3",
-        icon: SiCplusplus,
-        name: "C++",
+        icon:  FaPhp,
+        name: "php",
       },
       {
         id: "pl-4",
@@ -274,8 +182,8 @@ export const skills = [
       },
       {
         id: "pl-7",
-        icon: SiC,
-        name: "C",
+        icon:  FaNode,
+        name: "Node.js",
       },
       {
         id: "pl-8",
@@ -337,11 +245,7 @@ export const skills = [
         icon: SiGraphql,
         name: "GraphQL",
       },
-      {
-        id: "f-8",
-        icon: SiDotnet,
-        name: ".NET",
-      },
+      
     ],
   },
   {
@@ -369,8 +273,8 @@ export const skills = [
       },
       {
         id: "t-5",
-        icon: SiGit,
-        name: "Git",
+        icon:  FaWordpress,
+        name: "Wordpress",
       },
       {
         id: "t-6",
@@ -379,8 +283,8 @@ export const skills = [
       },
       {
         id: "t-7",
-        icon: AiOutlineGitlab,
-        name: "Gitlab",
+        icon: SiWoo,
+        name: "Woo-commerce",
       },
       {
         id: "t-8",
