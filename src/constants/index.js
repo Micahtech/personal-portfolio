7eb2b087e97bb3832d950d225ed97f8e66fc9cc4
Micahtech <wisdomjohn1919@gmail.com>
2025-04-23 21:14:52 +0100

    bug fixing

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ import {
   placeicon,
   recruitment,
   huntly,
-  fivenines,
+  oracle,
   comicify_ai,
   greentrust,
   averlon,
@@ -71,15 +71,13 @@ import {
   SiOpenai,
   SiGooglecloud,
   SiNextdotjs,
-  SiMicrosoftazure,
-  SiWoo
+  SiMicrosoftazure
 } from "react-icons/si";
 
 import {
   FaHardHat,
   FaRust,
-  FaPhp,
-  FaWordpress
+  FaPhp
 } from "react-icons/fa";
 
 import {
@@ -87,8 +85,7 @@ import {
 } from "react-icons/io";
 
 import {
-  FaGolang,
-  FaNode
+  FaGolang
 } from "react-icons/fa6";
 
 
@@ -149,6 +146,93 @@ export const educationList = [
 ];
 
 // Add your past achievments here for example - rankings in hackathons/events
+export const achievements = [
+  {
+    id: "a-1",
+    icon: ethindia,
+    event: "ETHIndia'24 | India's Largest Ethereum Hackathon",
+    position: "Winner",
+    content1: "True Network's Winner, Polkadot's 1st Runner Up",
+    content2: "Lit Protocol's 2nd Runner Up",
+    content3: "Coinbase Pool Prize, Base Top 10",
+    article: "https://www.linkedin.com/posts/mittal-parth_super-stoked-to-announce-that-our-team-emerged-activity-7274735259621961729-tkq4?utm_source=share&utm_medium=member_desktop",
+    project: "https://devfolio.co/projects/khoj-3336",
+    youtube: "https://www.youtube.com/live/qJ4OCtnvjUY?si=VkcnHEdwJTEEDlMg&t=4718",
+  },
+  {
+    id: "a-2",
+    icon: ethglobal,
+    event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
+    position: "Winner",
+    content1: "Top 12 winners among 20k+ registrations",
+    content2: "One of Polygon's Best Public Goods",
+    content3: "Best Module on Biconomy SDK",
+    article: "https://www.thehindu.com/news/cities/Mangalore/nitk-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
+    project: "https://devfolio.co/projects/chargeswap-3527",
+    youtube: "https://youtu.be/9rieTya8Yds?t=3908",
+  },
+  {
+    id: "a-3",
+    icon: polkadot,
+    event: "Polkadot Hackathon: Europe Edition",
+    position: "2nd Runner Up in the ink! Smart Contract Category",
+    content1: "Built GreenTrust offering a novel solution for obtaining certification in organic farming.",
+    content2: "",
+    content3: "",
+    github: "https://github.com/pranav2305/GreenTrust",
+  },
+  {
+    id: "a-4",
+    icon: lightspeed,
+    event: "Warpspeed by Lightspeed",
+    position: "1st Runner Up",
+    content1: "1st Runner Up Overall by Lightspeed among 107 hackers",
+    content2: "Top 3 projects using Replit",
+    content3: "1st Runner Up by Amazon Web Services (AWS)",
+    article: "https://shorturl.at/fhjsT",
+  },
+  {
+    id: "a-5",
+    icon: dennisivy,
+    event: "September Hackathon by Dennis Ivy",
+    position: "Winner",
+    content1: "Rated the best portfolio website among 450+ participants across the globe.",
+    content2: "",
+    content3: "",
+    youtube: "https://www.youtube.com/watch?v=X2473En3h_o&t=5278s",
+    project: "https://parthmittal.netlify.app/",
+  },
+  {
+    id: "a-6",
+    icon: manipal,
+    event: "Manipal Hackathon'22",
+    position: "Consolation Prize",
+    content1: "Top 10 among 500+ teams across India",
+    content2: "Developed a cross-platform mobile application to address the problem of social cohesion.",
+    content3: "",
+    article: "https://shorturl.at/exEIQ",
+  },
+  {
+    id: "a-7",
+    icon: icon,
+    event: "ICON Hyperbuild Hackathon",
+    position: "Honorable Mention",
+    content1: "Honorable Mention among 655 participants across the globe in a 3 month-long online hackathon.",
+    content2: "",
+    content3: "",
+    project: "https://devpost.com/software/green-trust-xj2w6g",
+  },
+  {
+    id: "a-8",
+    icon: ethforall,
+    event: "ETHForAll 2023",
+    position: "Top 3 Superfluid Projects",
+    content1: "Bounty winners among 430 projects in ETHGlobal's largest online hackathon.",
+    content2: "",
+    content3: "",
+    project: "https://devfolio.co/projects/green-trust-ed14",
+  },
+];
 
 // Add your software developments skills here for example - programming languages, frameworks etc.
 export const skills = [
@@ -156,14 +240,19 @@ export const skills = [
     title: "Programming Languages",
     items: [
       {
+        id: "pl-1",
+        icon: DiRuby,
+        name: "Ruby",
+      },
+      {
         id: "pl-2",
         icon: SiPython,
         name: "Python",
       },
       {
         id: "pl-3",
-        icon:  FaPhp,
-        name: "php",
+        icon: SiCplusplus,
+        name: "C++",
       },
       {
         id: "pl-4",
@@ -182,8 +271,8 @@ export const skills = [
       },
       {
         id: "pl-7",
-        icon:  FaNode,
-        name: "Node.js",
+        icon: SiC,
+        name: "C",
       },
       {
         id: "pl-8",
@@ -245,7 +334,11 @@ export const skills = [
         icon: SiGraphql,
         name: "GraphQL",
       },
-      
+      {
+        id: "f-8",
+        icon: SiDotnet,
+        name: ".NET",
+      },
     ],
   },
   {
@@ -273,8 +366,8 @@ export const skills = [
       },
       {
         id: "t-5",
-        icon:  FaWordpress,
-        name: "Wordpress",
+        icon: SiGit,
+        name: "Git",
       },
       {
         id: "t-6",
@@ -283,8 +376,8 @@ export const skills = [
       },
       {
         id: "t-7",
-        icon: SiWoo,
-        name: "Woo-commerce",
+        icon: AiOutlineGitlab,
+        name: "Gitlab",
       },
       {
         id: "t-8",
@@ -303,16 +396,16 @@ export const skills = [
 // Add your current/past professional work experience here
 export const experiences = [
   {
-    organisation: "FiveNines Consulting, USA",
-    logo: fivenines,
-    link: "https://www.fiveninesconsulting.io",
+    organisation: "Oracle, India",
+    logo: oracle,
+    link: "https://www.oracle.com/in/",
     positions: [
       {
-        title: "Web Developer/ Devops Team Member",
-        duration: "Jan 2025 - Present",
+        title: "Member of Technical Staff - 1",
+        duration: "Jul 2024 - Present",
         content: [
           {
-            text: "Working as Web Developer and software developer for fivenines Consulting.",
+            text: "Working in the Database as a Service Control Plane team for Oracle Cloud Infrastructure.",
             link: "",
           },
         ],
