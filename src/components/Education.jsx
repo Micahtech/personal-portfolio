e34b9781fc6ab5658c4c30adede1d14edc1a1157
Micahtech <wisdomjohn1919@gmail.com>
2025-04-24 10:28:49 +0100

    responsive

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -28,7 +28,7 @@ const FeatureCard = ({
 	${index === educationList.length - 1 ? "mb-0" : "mb-6"} feature-card`}
   >
     <div
-      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      className={`w-[100px] h-[100px] rounded-full ${styles.flexCenter} bg-dimBlue`}
     >
       <img src={icon} alt="icon" className="w-[80%] h-[80%] object-contain" />
     </div>
